0a43b9d8efdd161df9765ba20809a104c44d50dd
Rick Chi <wenzhongchi@gmail.com>
2024-10-23 16:50:05 -0400

    fix: update main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import * as session from 'express-session';
-import * as cookieParser from 'cookie-parser';
 
 import { SentryInterceptor } from '@interceptors/sentry.interceptor';
 import { TransformInterceptor } from '@interceptors/transform.interceptor';
 import { setupSwagger } from '@swagger/setup';
-import * as passport from 'passport';
 
 import { AppModule } from './app.module';
 
@@ -22,28 +19,12 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalInterceptors(new SentryInterceptor());
 
-  app.use(
-    session({
-      secret: configService.get<string>('SESSION_SECRET'),
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(cookieParser());
-
   const origin = [];
   if (process.env.APP_ENV === 'dev') {
     origin.push('http://localhost:3000');
   }
 
   const allowedHeaders = ['Accept', 'Content-Type', 'Authorization'];
-  if (process.env.APP_ENV === 'dev') {
-    allowedHeaders.push('Baggage');
-    allowedHeaders.push('Sentry-Trace');
-  }
 
   const cors = {
     origin,
